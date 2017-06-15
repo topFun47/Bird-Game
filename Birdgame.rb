b0f60bbff2d18e51204a7a921d4fa9a58d176167
topFun47 <pavel.pstepanov@gmail.com>
2017-06-15 00:11:53 -0400

    Bird Game

---
+++ Birdgame.rb
@@ -0,0 +1,28 @@
+#
+# Bird Game
+# Programmed By: Paul Stepanov
+#
+
+def bird
+  if bird
+    true
+  else
+    false
+end
+
+
+
+score=0
+bird=false
+room=1
+alive=false
+
+
+puts "#{score}"
+puts "#{room}"
+puts "#{alive}"
+puts "#{bird}"
+
+if bird puts "Watch out there's a bird"
+end
+end
